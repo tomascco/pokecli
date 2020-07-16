a0100c7105045293197d8cba1602df5705825c22
Tomás Coelho <tomascoelho6@gmail.com>
2020-07-15 22:10:04 -0300

    Change requires' location

--- lib/pokecli.rb
+++ lib/pokecli.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require 'pokecli/version'
+require 'step/format_params'
+require 'step/compose_url'
+require 'step/perform_request'
 
 module Pokecli
   class Error < StandardError; end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,8 +2,6 @@
 
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'pokecli'
-require 'step/format_params'
-require 'step/compose_url'
 
 require 'minitest/autorun'
 require 'minitest/pride'
