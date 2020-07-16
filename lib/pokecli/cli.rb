12d2f0dbded4d9a465f6d88ee244627373c0f8cd
Tomás Coelho <tomascoelho6@gmail.com>
2020-07-15 22:11:21 -0300

    Add testing code

--- lib/pokecli/cli.rb
+++ lib/pokecli/cli.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 require 'thor'
+require 'pokecli'
 
 module Pokecli
   class CLI < Thor
-    desc 'ENTITY NAME', 'queries for an entity named NAME'
+    
+    desc 'query ENTITY NAME', 'queries for an entity named NAME'
     def query(entity, *name_args)
+      p Step::FormatParams.({entity: entity, name_args: name_args})
+          .then(Step::ComposeURL)
+          .then(Step::PerformRequest)
     end
   end
 end
