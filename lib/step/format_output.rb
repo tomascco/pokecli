fbf115e9a643a0b0fea6d97b2ebaf86c6b2f88ec
Tomás Coelho <tomascoelho6@gmail.com>
2020-10-30 16:02:55 -0300

    ✨ Finish ability formatter

--- lib/step/format_output.rb
+++ lib/step/format_output.rb
@@ -4,19 +4,33 @@ require 'u-case'
 
 module Step
   class FormatOutput < Micro::Case
-    attributes :response, :entity
+    attributes :response, :entity, :name
+    PrettifyName = ->(name) { name.tr('-', ' ').capitalize }
 
     def call!
       Success result: {data: method(entity).call}
     end
 
+    private
+
     def pokemon
     end
 
     def ability
-      effect_entries = response[:effect_entries].find { |effect| effect.dig(:language, :name) == 'en' }
+      effect = response[:effect_entries]
+        .find { |effect_object| effect_object.dig(:language, :name) == 'en' }
+        .fetch(:effect)
+      learning_pokemon = response[:pokemon]
+        .map { |ability| PrettifyName.call(ability.dig(:pokemon, :name)) }
+        .join("\n")
 
-      pokemon = response[:pokemon].map { |ability| ability.dig(:pokemon, :name) }
+      <<~POKEMON
+        Ability: #{PrettifyName.call(name)}
+        Effect in battle: #{effect}
+
+        Pokemon that learn this ability:
+        #{learning_pokemon}
+      POKEMON
     end
   end
 end
