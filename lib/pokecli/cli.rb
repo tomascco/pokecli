80c9f449a3076dbd83143e3be5347726b1f6f256
Tomás Coelho <tomascoelho6@gmail.com>
2020-11-17 17:49:23 -0300

    improve CLI class

--- lib/pokecli/cli.rb
+++ lib/pokecli/cli.rb
@@ -6,18 +6,20 @@ module Pokecli
   class CLI < Thor
     desc 'pokemon POKEMON', 'queries for an pokemon named POKEMON'
     def pokemon(*name_args)
-      result = 
-        GetEntityData::Flow.call(entity: :pokemon, name_args: name_args)
-
-      puts result[:data]
+      get_entity_data(entity: :pokemon, name_args: name_args)
     end
 
     desc 'ability ABILITY', 'queries for an ability named ABILITY'
     def ability(*name_args)
-      result =
-        GetEntityData::Flow.call(entity: :ability, name_args: name_args)
+      get_entity_data(entity: :ability, name_args: name_args)
+    end
 
-      puts result[:data]
+    private
+
+    def get_entity_data(entity:, name_args:)
+      GetEntityData::Flow.call(entity: entity, name_args: name_args)
+        .on_success { |result| puts result[:data] }
+        .on_failure { puts 'An error ocurred' }
     end
   end
 end
